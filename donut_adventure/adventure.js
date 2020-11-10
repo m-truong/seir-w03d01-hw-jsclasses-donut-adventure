f225841bac10f3260dfffeda7aa9da924b32296d
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-11-09 19:53:16 -0500

    dougie and pizzarat have fought

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -1,7 +1,7 @@
 // console.log("Check 123");
 
 class Hero {
-    constructor (name) {
+    constructor(name) {
         this.name = name;
         this.health = 100;
         this.weapons = {
@@ -17,19 +17,24 @@ class Hero {
     announceHealth() {
         console.log(this.health);
     }
-    fight() {
+    fight(enemy) {
         console.log('i\'m ready to rumble');
+        const weaponArr = Object.keys(this.weapons);
+        const randomNum = Math.floor(Math.random() * weaponArr.length);
+        const currWeapon = weaponArr[randomNum];
+        enemy.health -= currWeapon;
+        console.log(`${enemy.name} got hit by ${currWeapon}! His health is now ${enemy.health}`);
     }
 }
 /**
  * Create instance of Dougie
  */
 const dougie = new Hero("Dougie");
-console.log(dougie);
-dougie.talkSass();
+// console.log(dougie);
+// dougie.talkSass();
 
 class Enemy {
-    constructor (name) {
+    constructor(name) {
         this.name = name;
         this.health = 100;
         this.weapons = {
@@ -45,13 +50,60 @@ class Enemy {
     announceHealth() {
         console.log(this.health);
     }
-    fight() {
+    fight(enemy) {
         console.log('i\'m gonna flatten you like a slice of pepperoni!');
+        const weaponArr = Object.keys(this.weapons);
+        const randomNum = Math.floor(Math.random() * weaponArr.length);
+        const currWeapon = weaponArr[randomNum];
+        enemy.health -= currWeapon;
+        console.log(`${enemy.name} got hit by ${currWeapon}! His health is now ${enemy.health}`);
     }
 }
 /**
  * Created instance of Pizza Rat
  */
 const pizzaRat = new Enemy("PizzaRat");
-console.log(pizzaRat);
+// console.log(pizzaRat);
+// pizzaRat.talkSmack();
+
+/**
+ * Walking Down The Street
+ */
+
+/**
+ * 1. Have Dougie talkSass
+ */
+
+dougie.talkSass();
+
+/**
+ * 2. Have PizzaRat talkSmack
+ */
 pizzaRat.talkSmack();
+
+/**
+ * 3. Have Dougie announceHealth
+ */
+dougie.announceHealth();
+
+/**
+ * 4. Have PizzaRat announceHealth
+ */
+pizzaRat.announceHealth();
+
+// ======== Fight! ======== //
+
+/**
+ * 1. Have PizzaRat fight Dougie
+ */
+pizzaRat.fight(dougie);
+/**
+ * 2. Have Dougie fight Pizzar Rat
+ */
+dougie.fight(pizzaRat);
+
+/** 
+ * 3. Have Pizza Rat and Dougie both announceHealth to make sure their health as decreased!
+ */
+pizzaRat.announceHealth();
+dougie.announceHealth();
